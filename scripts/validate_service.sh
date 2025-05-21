9458ddd06ca8a76de902d98046f9e0c55054f107
ca14189 <73096967+ca14189@users.noreply.github.com>
2025-05-21 15:40:01 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "Validating the application is running..."
-APP_PID=$(pgrep -f 'sportthon.jar')
+echo "Validating deployment..."
 
-if [ -z "$APP_PID" ]; then
-  echo "Application is not running!"
-  exit 1
+# Check if the app is running (you can replace this with a health endpoint check)
+pgrep -f '*.jar' > /dev/null
+
+if [ $? -eq 0 ]; then
+  echo "Validation successful: Application is running."
+  exit 0
 else
-  echo "Application is running with PID $APP_PID"
+  echo "Validation failed: Application is not running."
+  exit 1
 fi
