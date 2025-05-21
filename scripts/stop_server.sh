196451d255293be051b57419404a436cc061b937
ca14189 <73096967+ca14189@users.noreply.github.com>
2025-05-21 15:39:38 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "Stopping any running Java app..."
-pkill -f 'sportthon.jar' || true
+echo "Stopping Java application..."
+
+# Kill any process running your JAR
+pkill -f '*.jar' || true
+
+echo "Application stopped."
