01a50aabe68c667ea53142789d501ee99da3069e
ca14189 <73096967+ca14189@users.noreply.github.com>
2025-05-21 16:11:22 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,8 +3,8 @@ echo "Starting Java application..."
 
 cd /home/ubuntu/app
 
-# Find the JAR file and start it in the background
-JAR_FILE=$(ls target/*.jar | head -n 1)
+# Find the JAR file in current directory
+JAR_FILE=$(ls *.jar | head -n 1)
 
 # Run the JAR file in the background
 nohup java -jar "$JAR_FILE" > app.log 2>&1 &
