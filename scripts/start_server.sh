b7bff970c3487662d7f351dc2dd127510ac546d4
ca14189 <73096967+ca14189@users.noreply.github.com>
2025-05-21 16:14:05 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,10 +3,7 @@ echo "Starting Java application..."
 
 cd /home/ubuntu/app
 
-# Find the JAR file in current directory
-JAR_FILE=$(ls *.jar | head -n 1)
-
-# Run the JAR file in the background
-nohup java -jar "$JAR_FILE" > app.log 2>&1 &
+# Run the known JAR file
+nohup java -jar sportthon.jar > app.log 2>&1 &
 
 echo "Application started."
